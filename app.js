71cd9c78d70ffdea8c02b381c5ca4271a41e96a1
Nos <zvezdazanatih@gmail.com>
2025-05-07 11:45:39 +0700

    18

--- app.js
+++ app.js
@@ -19,10 +19,7 @@ app.get("/", function(req, res){
         });
     });
 });
-// возвращаем форму для добавления данных
-app.get("/create", function(req, res){
-    res.render("create.hbs");
-});
+
 // получаем отправленные данные и добавляем их в БД 
 app.post("/create", urlencodedParser, function (req, res) {
     if(!req.body) return res.sendStatus(400);
@@ -33,35 +30,7 @@ app.post("/create", urlencodedParser, function (req, res) {
         res.redirect("/");
     });
 });
-// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
-app.get("/edit/:id", function(req, res){
-    const id = req.params.id;
-    pool.query("SELECT * FROM tab1 WHERE id=?", [id], function(err, data) {
-        if(err) return console.log(err);
-        res.render("edit.hbs", {
-            user: data[0]
-        });
-    });
-});
-// получаем отредактированные данные и отправляем их в БД
-app.post("/edit", urlencodedParser, function (req, res) {
-    if(!req.body) return res.sendStatus(400);
-    const name = req.body.name;
-    const age = req.body.age;
-    const id = req.body.id;
-    pool.query("UPDATE tab1 SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
-        if(err) return console.log(err);
-        res.redirect("/");
-    });
-});
-// получаем id удаляемого пользователя и удаляем его из бд
-app.post("/delete/:id", function(req, res){
-    const id = req.params.id;
-    pool.query("DELETE FROM tab1 WHERE id=?", [id], function(err, data) {
-        if(err) return console.log(err);
-        res.redirect("/");
-    });
-});
+
 app.listen(30333, function(){
     console.log("Сервер ожидает подключения...");
 });
