c30873a8b3a26d4aa7f92b2ca947bae98d28a855
Nos <zvezdazanatih@gmail.com>
2025-05-09 11:49:15 +0700

    50

--- app.js
+++ app.js
@@ -50,22 +50,23 @@ app.post("/create", urlencodedParser, function (req, res) {
 
 // с помощью колбэков (требуется mysql2)
 app.get("/kuku", function(req, res){
-    pool.query("SELECT * FROM tab1", function(err, data) {        
-        res.send(`<b style="font-size:50px; color:green">blablabla!!!<br> ${JSON.stringify(data)}</b>`);
+    pool.query("SELECT * FROM tab1", function(err, data) { 
+
+        res.send(`<b style="font-size:50px; color:green">blablabla!!!<br> ${JSON.stringify(data)[0]}</b>`);
     });
 });
 
 // с помощью промисов (требуется mysql2/promise)
 app.get("/bubu", function(req, res){
     pool2.query("SELECT * FROM tab1").then(function(data) {
-        res.send(`<b style="font-size:50px; color:blue"> heaven <br> ${JSON.stringify(data)}</b>/`);
+        res.send(`<b style="font-size:50px; color:blue"> heaven <br> ${JSON.stringify(data)[0]}</b>/`);
     });
 });
 
 // с помощью async - await (требуется mysql2/promise)
 app.get("/lulu", async function(req, res){
     let data=await pool2.query("SELECT * FROM tab1");    
-    res.send(`<b style="font-size:50px; color:red"> fire!!!<br> fire!!! <br> ${JSON.stringify(data)} </b>/`);
+    res.send(`<b style="font-size:50px; color:red"> fire!!!<br> fire!!! <br> ${JSON.stringify(data)[0]} </b>/`);
     });
 
 
